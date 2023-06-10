6ad207be2d88730eec0c8ae025867f0fd0fcbdbc
shaliniganeshan <shaliniganeshan2000@gmail.com>
2023-06-10 23:04:51 +0530

    read me updated

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Flask_assignments
+
+# Flask Assignment
+'''
+Address book app by using flask and python
+'''
+### API Reference
+
+#### Get all items
+
+```http
+  GET http://127.0.0.1:9999/show_addresses
+```
+
+#### Get item
+
+```http
+  GET http://127.0.0.1:9999/show_addresses/<_id>
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to fetch |
+
+#### Post item
+
+```http
+  POST http://127.0.0.1:9999/addresses
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `address`      | `string` | **Required**. Address details  |
+| `address_no`      | `int` | **Required**. Door number |
+| `latitude`      | `int` | **Required**.  latitude value|
+| `longitude`      | `int` | **Required**. longitude value |
+
+
+#### Put item
+
+```http
+  PUT http://127.0.0.1:9999/addresses/<_id>
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to fetch |
+| `address`      | `string` | **Required**. Address details  |
+| `address_no`      | `int` | **Required**. Door number |
+| `latitude`      | `int` | **Required**.  latitude value|
+| `longitude`      | `int` | **Required**. longitude value |
+
+##### which key need to update change that
+
+#### Delete item
+
+```http
+  DELETE http://127.0.0.1:9999/addresses/<_id>
+```
+
+| Parameter | Type     | Description                       |
+| :-------- | :------- | :-------------------------------- |
+| `id`      | `string` | **Required**. Id of item to fetch |
+
+## Screenshots
+
+![Post methode output](https://github.com/shaliniganeshan/Flask_assignments/blob/master/create_new_add_op.png)
+
+![Get methode output](https://github.com/shaliniganeshan/Flask_assignments/blob/master/show_add_op.png)
+
+![Delete methode output](https://github.com/shaliniganeshan/Flask_assignments/blob/master/del_add_op.png)
+
+## Authors
+
+- [@shaliniganesan](https://github.com/shaliniganeshan)
+
--- main.py
+++ main.py
@@ -5,8 +5,11 @@
 # import libraries
 import flask
 import sqlite3
+from loguru import logger
+
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+from sql import create_addresses_table
 import json
 
 # app
@@ -15,6 +18,7 @@ app = flask.Flask(__name__)
 # SQLite path
 db_path = 'address.db'
 
+#create_addresses_table() # create address db
 # db connection establish
 def conn_establish():
     conn = sqlite3.connect(db_path)
@@ -29,58 +33,78 @@ def conn_close(conn):
 # create a address function
 @app.route('/addresses', methods=['POST'])
 def create_address():
-    data = flask.request.get_json()
-    address = data['address']
-    address_no=data['address_no']
-    # Validating
-    geolocator = Nominatim(user_agent='addressbook')
-    location = geolocator.geocode(address)
-    if not location:
-        return flask.jsonify({'message': 'Invalid address'}), 400
-    conn = conn_establish()
-    cursor = conn.cursor()
-    cursor.execute('''
-        INSERT INTO addresses (address, address_no, latitude, longitude)
-        VALUES (?, ?, ?, ?)
-    ''', (address, address_no, location.latitude, location.longitude))
-    conn.commit()
-    conn_close(conn)
+    try:
+        data = flask.request.get_json()
+        address = data['address']
+        address_no=data['address_no']
+        logger.info(data)
+        # Validating
+        geolocator = Nominatim(user_agent='addressbook')
+        location = geolocator.geocode(address)
+        logger.info(location)
+        if not location:
+            return flask.jsonify({'message': 'Invalid address'}), 400
+        conn = conn_establish()
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT INTO addresses (address, address_no, latitude, longitude)
+            VALUES (?, ?, ?, ?)
+        ''', (address, address_no, location.latitude, location.longitude))
+        conn.commit()
+        conn_close(conn)
+        message={'message': 'Address created successfully'}
+        status_code =201
+    except:
+        message={'message': 'Address not created'}
+        status_code =400
+    return flask.jsonify(message),status_code
 
-    return flask.jsonify({'message': 'Address created successfully'}), 201
 
 # update existed address
 @app.route('/addresses/<int:_id>', methods=['PUT'])
 def update_address(_id):
-    data = flask.request.get_json()
-    address = data['address']
-    address_no=data["address_no"]
+    try:
+        data = flask.request.get_json()
+        address = data['address']
+        address_no=data["address_no"]
+        # Validate the address
+        geolocator = Nominatim(user_agent='addressbook')
+        location = geolocator.geocode(address)
+        if not location:
+            return flask.jsonify({'message': 'Invalid address'}), 400
 
-    # Validate the address
-    geolocator = Nominatim(user_agent='addressbook')
-    location = geolocator.geocode(address)
-    if not location:
-        return flask.jsonify({'message': 'Invalid address'}), 400
+        conn = conn_establish()
+        cursor = conn.cursor()
+        cursor.execute('''
+            UPDATE addresses
+            SET address = ?, address_no = ?, latitude = ?, longitude = ?
+            WHERE id = ?
+        ''', (address, address_no, location.latitude, location.longitude, _id))
+        conn.commit()
+        conn_close(conn)
+        message={'message': 'Address updated successfully'}
+        status_code =201
+    except:
+        message={'message': 'Address not updated as expected'}
+        status_code =400
+    return flask.jsonify(message),status_code
 
-    conn = conn_establish()
-    cursor = conn.cursor()
-    cursor.execute('''
-        UPDATE addresses
-        SET address = ?, address_no = ?, latitude = ?, longitude = ?
-        WHERE id = ?
-    ''', (address, address_no, location.latitude, location.longitude, _id))
-    conn.commit()
-    conn_close(conn)
-    return flask.jsonify({'message': 'Address updated successfully'})
 
 # delete existed address
 @app.route('/addresses/<int:_id>', methods=['DELETE'])
 def delete_address(_id):
-    conn = conn_establish()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM addresses WHERE id = ?', (_id,))
-    conn.commit()
-    conn_close(conn)
-    return flask.jsonify({'message': 'Address deleted successfully'})
+    try:
+        conn = conn_establish()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM addresses WHERE id = ?', (_id,))
+        conn.commit()
+        conn_close(conn)
+        message={'message': 'Address deleted successfully'}
+        status_code =201
+    except:
+        message={'message': 'Address not deleted as expected'}
+        status_code =400
+    return flask.jsonify(message),status_code
 
 
 # show all address
@@ -91,7 +115,7 @@ def show_addresses():
     addresses=cursor.execute('SELECT * FROM addresses') 
     rows = cursor.fetchall()
     results = [] # results list
-    print(rows)
+    logger.info(rows)
     # iterate the row
     for row in rows:
         # Create a dictionary for each row
@@ -104,36 +128,40 @@ def show_addresses():
         }
         # Append the all dictionary to the results 
         results.append(row_dict)
-    print(results)
+    logger.info(results)
     conn_close(conn)
     json_addresses = json.dumps(results, indent=4, sort_keys=True)
-    print(json_addresses)
+    logger.info(json_addresses)
     return json_addresses #return formated address
 
 
 @app.route('/show_addresses/<int:_id>', methods=['GET'])
 def show_addresses_separate(_id):
-    conn = conn_establish()
-    cursor = conn.cursor()
-    addresses=cursor.execute('SELECT * FROM addresses WHERE id = ?', (_id,))
-    rows = cursor.fetchall()
-    print(addresses)
-    results = []
-    json_addresses=[]
-    # Iterate the row
-    for row in rows:
-        row_dict = {
-            'id': row[0],
-            'address_no': row[1],
-            'address': row[2],
-            'latitude':row[3],
-            'longitude':row[4]
-        }
-        results.append(row_dict)
-        print(results)
-        conn_close(conn)
-        json_addresses = json.dumps(results, indent=4, sort_keys=True)
-        print(json_addresses)
+    try:
+        conn = conn_establish()
+        cursor = conn.cursor()
+        addresses=cursor.execute('SELECT * FROM addresses WHERE id = ?', (_id,))
+        rows = cursor.fetchall()
+        logger.info(addresses)
+        results = []
+        
+        # Iterate the row
+        for row in rows:
+            row_dict = {
+                'id': row[0],
+                'address_no': row[1],
+                'address': row[2],
+                'latitude':row[3],
+                'longitude':row[4]
+            }
+            results.append(row_dict)
+            logger.info(results)
+            conn_close(conn)
+            json_addresses = json.dumps(results, indent=4, sort_keys=True)
+            logger.info(json_addresses)
+    except:
+        json_addresses=[]
+
     return json_addresses  #return formated address
 
 
